bea1c3b1c8525c707b55b601f412e1a2cfc21da1
NoOPeEKS <arnauapps@gmail.com>
2024-10-01 10:16:02 +0200

    Add one more question to pipeline

--- src/pipelines/chat.py
+++ src/pipelines/chat.py
@@ -4,3 +4,4 @@ from src.models.rag import RAG
 def execute(settings: dict, global_settings: dict):
     rag = RAG(settings)
     print(rag.answer_question("How did juliet die?").split("\n")[-1])
+    print(rag.answer_question("From which family is Romeo?").split("\n")[-1])
