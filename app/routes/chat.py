14a1c8a53a062ff4a888cd2861d269bea5dea1b5
NoOPeEKS <arnauapps@gmail.com>
2024-10-01 10:28:40 +0200

    Document endpoint

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.tools.startup import params
+from src.tools.startup import params, logger
 from src.models.rag import RAG
 
 
@@ -14,12 +14,17 @@ router = APIRouter(
         200: {"Description": "The answer to the user requested question"}})
 def answer_query(query: str):
     """
-    Uses RAG to answer the given query
+    Answers the given query question using the RAG agent.
+
+    Args:
+        query(str): The question to be answered
+
+    Returns:
+        answer(JSON): The answer to the question
     """
 
+    logger.info(f"Started answering question '{query}'")
     answer = rag.answer_question(query).split("\n")[-1]
+    logger.info("Answered question")
 
     return {"answer": answer}
-
-
-
